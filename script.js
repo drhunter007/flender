a49a5ca2a752db32a0d941ce21509bd118422791
Ben9959733 <Benheju.sun@gmail.com>
2025-06-26 10:44:32 +0200

    sites

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function funktion1(){
     document.getElementById("überschrift1").style.color = "black";
-    document.getElementById("überschrift1").style.backgroundColor = "lightblue";
+    document.getElementById("überschrift2").style.color = "black";
+    document.getElementById("überschrift2").innerHTML ="Eine Helle Website";
+    document.getElementById("Zeile1").innerHTML ="";
+    document.getElementById("Zeile2").innerHTML ="";
+    document.getElementById("Zeile1").style.color = "black";
+    document.getElementById("Zeile2").style.color = "black";
+}
+function body1(){
+    document.getElementById("body1").style.backgroundColor= "white";
+    funktion1();
 }
