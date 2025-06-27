86570ecb1d5d2afe9ea3fc754cd740c2f0c1ca4d
Ben9959733 <Benheju.sun@gmail.com>
2025-06-27 12:48:16 +0200

    sites

--- script1.js
+++ script1.js
@@ -1,3 +1,6 @@
+let intervalId = null;
+let intervalId1 = null;
+let intervalId2 = null;
 function getRandomColor() {
     const colors = [
         "red", "green", "blue", "yellow", "orange",
@@ -5,15 +8,46 @@ function getRandomColor() {
     ];
     const index = Math.floor(Math.random() * colors.length);
     return colors[index];
+    
 }
 
 // Beispiel:
 
      
 function knopf1() {
-    let x = setInterval(getRandomColor,500);
+    if (intervalId === null) {
+        intervalId = setInterval(() => {
+            document.getElementById("div1").style.backgroundColor = getRandomColor();
+        }, 100);
+    }
 }
 function knopf2() {
-    clearInterval(x)
+    clearInterval(intervalId);
+    intervalId = null;
 }
+    
+function knopf3() {
+    if (intervalId1 === null) {
+        intervalId1 = setInterval(() => {
+            document.getElementById("div2").style.backgroundColor = getRandomColor();
+        }, 100);
+    }
+}
+function knopf4() {
+    clearInterval(intervalId1);
+    intervalId1 = null;
+}
+
+function knopf5() {
+    if (intervalId2 === null) {
+        intervalId2 = setInterval(() => {
+            document.getElementById("div3").style.backgroundColor = getRandomColor();
+        }, 100);
+    }
+}
+function knopf6() {
+    clearInterval(intervalId2);
+    intervalId2 = null;
+}
+    
     
